8229f27c9887ae4679757a45cde3272273e87ea1
Dmitry Ilyevsky <ilyevsky@gmail.com>
2023-08-10 09:50:02 -0700

    [tailscaled] Proxy based on Host header.

--- cmd/tailscaled/proxy.go
+++ cmd/tailscaled/proxy.go
@@ -13,6 +13,7 @@ import (
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
 	"strings"
 )
 
@@ -28,9 +29,16 @@ func httpProxyHandler(dialer func(ctx context.Context, netw, addr string) (net.C
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "CONNECT" {
 			backURL := r.RequestURI
-			if strings.HasPrefix(backURL, "/") || backURL == "*" {
+			if backURL == "*" {
 				http.Error(w, "bogus RequestURI; must be absolute URL or CONNECT", 400)
 				return
+			}
+			if strings.HasPrefix(backURL, "/") {
+				r.URL = url.URL{
+					Scheme: "http",
+					Host:   r.Host,
+					Path:   r.URL.Path,
+				}
 			}
 			rp.ServeHTTP(w, r)
 			return
